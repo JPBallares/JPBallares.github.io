e0e9d939243c48c0b053a0ce70fff8e0d5324c01
Justine Ballares <ballaresjustine@gmail.com>
2018-04-04 00:01:18 +0800

    fix

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
       console.log('Service Worker is registered', swReg);
 
       swRegistration = swReg;
-      initializeUI();
+      initializeUI;
     })
     .catch(function (error) {
       console.error('Service Worker Error', error);

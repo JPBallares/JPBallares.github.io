36ed5b3cd1ac8c1c2b6159d0fd8e64f0b4d0c5cb
Justine Ballares <ballaresjustine@gmail.com>
2018-04-06 20:08:13 +0800

    block access of data files in IIS and fix register

--- register.php
+++ register.php
@@ -1,12 +1,12 @@
 <html>
 <body>
 
-<form action="open_db.php" method="post">
+<form action="register.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
-Password: <input type="text" name="password"><br>
+Password: <input type="password" name="password"><br>
 Contact Number: <input type="text" name="number"><br>
-<input type="submit">
+<input type="submit" >
 </form>
 
 </body>

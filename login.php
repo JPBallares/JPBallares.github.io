8e0d105900711023df11b82d06ccb78143c0fd9b
Justine Ballares <ballaresjustine@gmail.com>
2018-04-06 19:32:47 +0800

    add login.php initial for login of manager

---
+++ login.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+
+
+
+    $user = 'webtech';
+    $pass = '66efd9eefecf45dd64eff8e5cb2d13e005041925';
+
+    if(isSet($_POST['login'])) {
+
+
+        $username = $_POST['username'];
+        $password = sha1($_POST['password'] );
+
+        if ($user == $username && $pass == $password) {
+            $_SESSION['username'] = $username;
+            $_SESSION['password'] = $password;
+
+            header('Location: /member_area.php');
+            exit;
+        } else {
+            echo 'Data does not match <br /> RE-Enter Username and Password';
+        }
+    } else {
+
+?>
+    <html>
+    <head><link rel="stylesheet" type="text/css" href="css.css"></head>
+        <body>
+                <div id="div1">
+
+                <a href="index.php" id="home">Home</a>
+                <a href="Login.php" id="login2">Login</a>
+                <a href="register.php" id="register">Register</a> 
+
+
+        </div>
+            <table width="200" border="0" cellspacing="1" align="center">
+                <form id="form1" method="post" action="login.php">
+                <tr>
+                    <td colspan="2"><h2>Members login</h2></td>
+                </tr>
+                <tr>
+                    <td>Username: </td>
+                    <td>
+                        <input type="text" name="username" id="username"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password: </td>
+                        <td><input type="password" name="password" id="password"/> </td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center">
+                        <input type="submit" name="login" id="login" value="login" />
+                    </td>
+                </tr>
+                </form>
+            </table>
+            </body>
+    </html>
+    <?php
+    }
+
+
+?>

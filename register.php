eae8b494d74be01a57c1ec23f4d06e4ec3db0140
Justine Ballares <ballaresjustine@gmail.com>
2018-04-06 19:34:19 +0800

    added register.php for registration if needed

---
+++ register.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="open_db.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="text" name="password"><br>
+Contact Number: <input type="text" name="number"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>

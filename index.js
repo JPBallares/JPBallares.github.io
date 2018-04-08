258aeae1d8d7e5378cb8a61dcb5ce8a182b96071
cs-13 <cs13.panawan@gmail.com>
2018-04-08 14:24:58 +0800

    Updated the UI for the bike management.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 window.onload = function () {
-	var addButton = document.getElementById('pluBu');
+	var addButton = document.getElementById('add-c');
 	var delButton = document.getElementById('delBu');
 	var listOfRents = document.querySelector('ul');
+	var modal = document.getElementById('modal-0');
+	var btn = document.getElementById('pluBu');
+	var span = document.getElementsByClassName("close")[0];
 	
 	addButton.onclick = function() {
 		var newCon = document.createElement('li');
@@ -19,5 +22,22 @@ window.onload = function () {
 		delButton.onclick = function(e) {
     		e.target.closest('li').remove()
   		}
+		
+		modal.style.display = "none";
+	}
+
+	btn.onclick = function() {
+		modal.style.display = "block";
+	}
+
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
 	}
 }
+
